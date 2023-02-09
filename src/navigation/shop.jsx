cee51af01d626ece75fe591359905f8599c02359
santi0995 <santiserrato0910@gmail.com>
2023-02-09 19:10:07 +0100

    Ultimos añadidos de cabecera

--- src/navigation/shop.jsx
+++ src/navigation/shop.jsx
@@ -1,22 +1,45 @@
 import { Categories, ProductDetail, Products } from '../screens/index';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { theme } from '../constants/theme';
 
 const Stack = createNativeStackNavigator();
 
 const ShopNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Categories">
+    <Stack.Navigator
+      initialRouteName="Categories"
+      screenOptions={{
+        headerTintColor: theme.colors.primary,
+        navigationBarColor: theme.colors.primary,
+        headerTitleStyle: {
+          fontFamily: 'Lobster-Regular',
+          fontSize: 18,
+          color: theme.colors.secondary,
+        },
+        headerShadowVisible: false,
+      }}>
       <Stack.Screen
         name="Categories"
         component={Categories}
-        options={{ headerShown: false, title: 'Categories' }}
+        options={{
+          headerShown: false,
+          title: 'Categories',
+        }}
       />
       <Stack.Screen
         name="Products"
         component={Products}
         options={{
           title: 'Products',
+          headerRight: () => {
+            return (
+              <TouchableOpacity>
+                <Text>Click here</Text>
+              </TouchableOpacity>
+            );
+          },
         }}
       />
       <Stack.Screen
